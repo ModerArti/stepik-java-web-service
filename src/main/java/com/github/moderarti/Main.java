446b36a562f513255a9be577267aab28ce59279b
Moder_Arti <moder_arti@mail.ru>
2020-06-07 21:14:39 +0300

    Add main

---
+++ src/main/java/com/github/moderarti/Main.java
@@ -0,0 +1,26 @@
+package com.github.moderarti;
+
+import com.github.moderarti.servlet.KeyValueServlet;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import java.util.logging.Logger;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        KeyValueServlet servlet = new KeyValueServlet();
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(servlet), "/mirror");
+
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+        Logger.getGlobal().info("Server started");
+        server.join();
+    }
+
+}
