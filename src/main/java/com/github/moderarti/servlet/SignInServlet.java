f20d67faa34420154eb060f5314c597f5b00ca0b
Moder_Arti <moder_arti@mail.ru>
2020-06-17 21:57:20 +0300

    Add doPost

--- src/main/java/com/github/moderarti/servlet/SignInServlet.java
+++ src/main/java/com/github/moderarti/servlet/SignInServlet.java
@@ -1,8 +1,12 @@
 package com.github.moderarti.servlet;
 
-import com.github.moderarti.accounts.AccountService;;
+import com.github.moderarti.accounts.AccountService;
+import com.github.moderarti.accounts.UserProfile;;
 
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class SignInServlet extends HttpServlet {
 
@@ -10,6 +14,23 @@ public class SignInServlet extends HttpServlet {
 
     public SignInServlet(AccountService accountService) {
         this.accountService = accountService;
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String login = req.getParameter("login");
+        String password = req.getParameter("pass");
+
+        UserProfile user;
+        if ((user = accountService.getUserByLogin(login)) != null) {
+            if (user.getPassword().equals(password)) {
+                resp.setStatus(HttpServletResponse.SC_OK);
+                resp.getWriter().println("Authorized: login");
+            }
+        } else {
+            resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            resp.getWriter().println("Unauthorized");
+        }
     }
 
 }
