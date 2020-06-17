8a0d53b42c66785ee86c397a24b826ece4a2282d
Moder_Arti <moder_arti@mail.ru>
2020-06-17 22:08:22 +0300

    Add using of SighInServlet and SignUpServlet in Main

--- src/main/java/com/github/moderarti/Main.java
+++ src/main/java/com/github/moderarti/Main.java
@@ -1,23 +1,38 @@
 package com.github.moderarti;
 
+import com.github.moderarti.accounts.AccountService;
 import com.github.moderarti.property.PropertiesENUM;
 import com.github.moderarti.property.PropertiesHandler;
-import com.github.moderarti.servlet.KeyValueServlet;
+import com.github.moderarti.servlet.SignInServlet;
+import com.github.moderarti.servlet.SignUpServlet;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
+import javax.servlet.http.HttpServlet;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
 public class Main {
 
     private static final Logger LOGGER = Logger.getLogger(Main.class.toString());
 
+    private static Map<HttpServlet, String> getServletsAndPages(AccountService accountService) {
+        Map<HttpServlet, String> servlets = new HashMap<>();
+        servlets.put(new SignUpServlet(accountService), "signup");
+        servlets.put(new SignInServlet(accountService), "signin");
+        return servlets;
+    }
+
     public static void main(String[] args) throws Exception {
-        KeyValueServlet servlet = new KeyValueServlet();
+        AccountService accountService = new AccountService();
+        Map<HttpServlet, String> servletsAndPages = getServletsAndPages(accountService);
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(servlet), "/mirror");
+        for (Map.Entry<HttpServlet, String> servletAndPage : servletsAndPages.entrySet()) {
+            context.addServlet(new ServletHolder(servletAndPage.getKey()), servletAndPage.getValue());
+        }
 
         int numberOfPort = Integer.parseInt(PropertiesHandler.getProperty(PropertiesENUM.PORT));
         Server server = new Server(numberOfPort);
