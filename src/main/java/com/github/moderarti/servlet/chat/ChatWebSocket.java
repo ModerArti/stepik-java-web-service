b4a010affb28755b825fbbeb73e95c7a076eabd0
Moder_Arti <moder_arti@mail.ru>
2020-07-22 21:20:13 +0300

    Add class ChatWebSocket

---
+++ src/main/java/com/github/moderarti/servlet/chat/ChatWebSocket.java
@@ -0,0 +1,32 @@
+package com.github.moderarti.servlet.chat;
+
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@WebSocket
+public class ChatWebSocket {
+
+    private final static Logger LOGGER = Logger.getLogger(ChatWebSocket.class.toString());
+
+    private Session session;
+
+    @OnWebSocketConnect
+    public void onOpen(Session session) {
+        this.session = session;
+    }
+
+    @OnWebSocketMessage
+    public void onMessage(String data) {
+        try {
+            session.getRemote().sendString(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
